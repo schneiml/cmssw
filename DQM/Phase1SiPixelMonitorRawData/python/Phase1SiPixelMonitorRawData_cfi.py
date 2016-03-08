3b58e18153bb6365be70f0fa256c220c2cb9b753
Marcel Andre Schneider <marcel.andre.schneider@cern.ch>
2016-03-08 12:01:45 +0100

    Renamed RawDataError Module

---
+++ DQM/Phase1SiPixelMonitorRawData/python/Phase1SiPixelMonitorRawData_cfi.py
@@ -0,0 +1,22 @@
+import FWCore.ParameterSet.Config as cms
+
+#
+# This object is used to make changes for different running scenarios
+#
+from Configuration.StandardSequences.Eras import eras
+
+Phase1SiPixelRawDataErrorSource = cms.EDAnalyzer("Phase1SiPixelRawDataErrorSource",
+    TopFolderName = cms.string('Pixel'),
+    src = cms.InputTag("siPixelDigis"),
+    outputFile = cms.string('Pixel_DQM_Error.root'),
+    saveFile = cms.untracked.bool(False),
+    isPIB = cms.untracked.bool(False),
+    slowDown = cms.untracked.bool(False),
+    reducedSet = cms.untracked.bool(False),
+    modOn = cms.untracked.bool(True),
+    ladOn = cms.untracked.bool(False),
+    bladeOn = cms.untracked.bool(False)
+)
+
+# Modify for if the phase 1 pixel detector is active
+eras.phase1Pixel.toModify( Phase1SiPixelRawDataErrorSource, isUpgrade=cms.untracked.bool(True) )
