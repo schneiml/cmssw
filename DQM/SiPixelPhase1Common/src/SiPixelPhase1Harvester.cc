9eeea34ceed108a247cde537527e5cb35a951732
Marcel Andre Schneider <marcel.andre.schneider@cern.ch>
2016-08-11 13:35:34 +0200

    Fix compilation/loading problems due to a plugin being declared in a library.

--- DQM/SiPixelPhase1Common/src/SiPixelPhase1Harvester.cc
+++ DQM/SiPixelPhase1Common/src/SiPixelPhase1Harvester.cc
@@ -1,5 +1,12 @@
 // This is only to declare a plugin, the implementation is in the header. 
 #include "DQM/SiPixelPhase1Common/interface/SiPixelPhase1Base.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-DEFINE_FWK_MODULE(SiPixelPhase1Harvester);
 
+void SiPixelPhase1Harvester::dqmEndLuminosityBlock(DQMStore::IBooker& iBooker, DQMStore::IGetter& iGetter, edm::LuminosityBlock const& lumiBlock, edm::EventSetup const& eSetup) {
+  for (HistogramManager& histoman : histo)
+    histoman.executeHarvestingOnline(iBooker, iGetter, eSetup);
+};
+void SiPixelPhase1Harvester::dqmEndJob(DQMStore::IBooker& iBooker, DQMStore::IGetter& iGetter) {
+  for (HistogramManager& histoman : histo)
+    histoman.executeHarvestingOffline(iBooker, iGetter);
+};
