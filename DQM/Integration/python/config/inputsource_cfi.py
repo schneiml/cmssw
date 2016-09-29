4f7ff3c2163d96af17ca00b131d26bdcba47ba01
Marcel Andre Schneider <marcel.andre.schneider@cern.ch>
2016-09-29 11:59:45 +0200

    TEST: Online Setup changes to run on lxplus with data copied from online.

--- DQM/Integration/python/config/inputsource_cfi.py
+++ DQM/Integration/python/config/inputsource_cfi.py
@@ -51,7 +51,7 @@ if not options.runkey.strip():
 runType.setRunType(options.runkey.strip())
 
 # Input source
-nextLumiTimeoutMillis = 90000
+nextLumiTimeoutMillis = 10000
 endOfRunKills = True
 
 if options.scanOnce:
@@ -64,7 +64,7 @@ source = cms.Source("DQMStreamerReader",
     SelectEvents = cms.untracked.vstring('*'),
     streamLabel = cms.untracked.string('streamDQM'),
     scanOnce = cms.untracked.bool(options.scanOnce),
-    minEventsPerLumi = cms.untracked.int32(1),
+    minEventsPerLumi = cms.untracked.int32(100),
     delayMillis = cms.untracked.uint32(500),
     nextLumiTimeoutMillis = cms.untracked.int32(nextLumiTimeoutMillis),
     skipFirstLumis = cms.untracked.bool(options.skipFirstLumis),
