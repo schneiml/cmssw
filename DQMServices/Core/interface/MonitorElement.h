4fd8331383da141edb43e347dd448ad083af7b98
Marcel Andre Schneider <marcel.andre.schneider@cern.ch>
2019-02-25 17:42:03 +0100

    Some stuff compiles a bit

--- DQMServices/Core/interface/MonitorElement.h
+++ DQMServices/Core/interface/MonitorElement.h
@@ -1,1 +1,32 @@
+#ifndef DQMSERVICES_CORE_MONITORELEMENT_H
+#define DQMSERVICES_CORE_MONITORELEMENT_H
 
+# if __GNUC__ && ! defined DQM_DEPRECATED
+#  define DQM_DEPRECATED __attribute__((deprecated))
+# endif
+
+
+# include "TH1F.h"
+# include "TH1S.h"
+# include "TH1D.h"
+# include "TH2F.h"
+# include "TH2S.h"
+# include "TH2D.h"
+# include "TH3F.h"
+# include "TProfile.h"
+# include "TProfile2D.h"
+
+
+class MonitorElement {
+public:
+
+  void Fill(double x) {};
+  void Fill(double x, double y) {};
+  void Fill(double x, double y, double z) {};
+  void Fill(std::string value) {};
+
+  DQM_DEPRECATED void Reset();
+
+};
+
+#endif
