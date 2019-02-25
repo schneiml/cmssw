cb3d9126c0a437bdf204fa72ef505f6a81bffea0
Marcel Andre Schneider <marcel.andre.schneider@cern.ch>
2019-02-25 16:43:42 +0100

    Empty out DQMServices/Core, to grow something new.

--- DQMServices/Core/src/QStatisticalTests.h
+++ DQMServices/Core/src/QStatisticalTests.h
@@ -1,2 +1,1 @@
-void BinLogLikelihoodRatio(long, long, double, double*, double*);
-void PoissionLogLikelihoodRatio(double, double,	double, double,	double*, double* );
+
--- DQMServices/Core/test/DQMTestStandaloneBuildOfDQMStore.cc
+++ DQMServices/Core/test/DQMTestStandaloneBuildOfDQMStore.cc
@@ -1,7 +1,1 @@
-#define WITHOUT_CMS_FRAMEWORK 1
-#include "DQMServices/Core/src/DQMStore.cc"
 
-int main()
-{
-  return 0;
-}
