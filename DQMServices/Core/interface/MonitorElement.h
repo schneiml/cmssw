dd3952d5317be34abf7e40587f6c2d396f52b139
Marcel Andre Schneider <marcel.andre.schneider@cern.ch>
2019-02-26 19:02:37 +0100

    Add all MonitorElement APIs according to the API popularity list.

--- DQMServices/Core/interface/MonitorElement.h
+++ DQMServices/Core/interface/MonitorElement.h
@@ -32,28 +32,81 @@ public:
 
   // Other setters
   void setAxisTitle(std::string title, int axis = 0) {};
+  DQM_DEPRECATED void setXTitle(std::string title) {};
+  DQM_DEPRECATED void setYTitle(std::string title) {};
+  DQM_DEPRECATED void setAxisRange(double, double, int) {};
 
   // Maybe deprecated?
-  DQM_DEPRECATED void setBinLabel(int binindex, std::string label) {};
+  DQM_DEPRECATED void setBinLabel(int binindex, std::string label, int axis = 0) {};
 
   DQM_DEPRECATED void Reset() {};
 
   DQM_DEPRECATED TObject* getRootObject() { return nullptr; };
   DQM_DEPRECATED TH1* getTH1() { return nullptr; };
   DQM_DEPRECATED TH1F* getTH1F() { return nullptr; };
+  DQM_DEPRECATED TH1D* getTH1D() { return nullptr; };
   DQM_DEPRECATED TH2F* getTH2F() { return nullptr; };
+  DQM_DEPRECATED TH2D* getTH2D() { return nullptr; };
+  DQM_DEPRECATED TH2S* getTH2S() { return nullptr; };
+  DQM_DEPRECATED TProfile* getTProfile() { return nullptr; };
   DQM_DEPRECATED TProfile2D* getTProfile2D() { return nullptr; };
 
+  int getIntValue() { return 0; };
+  float getFloatValue() { return 0; };
+
+
   DQM_DEPRECATED std::string getTitle() const { return ""; };
+  DQM_DEPRECATED void setTitle(std::string) const {};
   DQM_DEPRECATED std::string getFullname() { return ""; };
+  DQM_DEPRECATED std::string getPathname() { return ""; };
+  DQM_DEPRECATED std::string getName() { return ""; };
 
   // non-histogram operations -- can we force people to use TProfile instead?
   DQM_DEPRECATED double getEntries() { return 0; };
   DQM_DEPRECATED void setEntries(double) {};
-  DQM_DEPRECATED void setEfficiencyFlag(bool set = true) {};
+  DQM_DEPRECATED void setBinEntries(int, double) {};
+  DQM_DEPRECATED double getBinEntries(int) { return 0; };
   DQM_DEPRECATED void setBinContent(int, double) {};
+  DQM_DEPRECATED void setBinContent(int, int, double) {};
   DQM_DEPRECATED void setBinError(int, double) {};
+  DQM_DEPRECATED double getBinError(int) { return 0; };
+  DQM_DEPRECATED void setBinError(int, int, double) {};
+  DQM_DEPRECATED double getBinError(int, int) { return 0; };
   DQM_DEPRECATED int getNbinsX() { return 0; };
+  DQM_DEPRECATED int getNbinsY() { return 0; };
+  DQM_DEPRECATED double getBinContent(int bin) { return 0; };
+  DQM_DEPRECATED double getBinContent(int, int) { return 0; };
+  DQM_DEPRECATED double getMean(int) { return 0; };
+  DQM_DEPRECATED double getMeanError(int) { return 0; };
+  DQM_DEPRECATED double getRMS(int) { return 0; };
+
+  DQM_DEPRECATED void setEfficiencyFlag(bool set = true) {};
+  DQM_DEPRECATED void setLumiFlag(bool set = true) {};
+
+  // QTest API
+  //QReport getQReport(std::string qtestname);
+  //std::vector<QReport> getQReports();
+  bool hasError() { return false; }
+  bool hasWarning() { return false; }
+  bool hasOtherReport() { return false; }
+
+  // almost unused things (BeamMonitor uses many)
+  DQM_DEPRECATED void setResetMe(bool) {};
+  DQM_DEPRECATED std::string valueString() { return ""; }
+  DQM_DEPRECATED TObject* getRefRootObject() { return nullptr; }
+  DQM_DEPRECATED TH1* getRefTH1() { return nullptr; }
+
+  DQM_DEPRECATED void update() {};
+  DQM_DEPRECATED void doFill(int64_t) {}; // ??
+  DQM_DEPRECATED void ShiftFillLast(double, double, int) {};
+  //std::vector<QReport> getQWarnings();
+  //std::vector<QReport> getQErrors();
+  DQM_DEPRECATED double getRMSError(int) { return 0; };
+  DQM_DEPRECATED std::string getAxisTitle(int) { return ""; };
+  DQM_DEPRECATED void setAxisTimeDisplay(int, int) {};
+  DQM_DEPRECATED void setAxisTimeFormat(std::string, int) {};
+  DQM_DEPRECATED void softReset() {};
+  // DQM_DEPRECATED getTags();
 
 
   // Maybe this should be deprecated as well, but there are a lot of users and it is not too bad to have.
