d07e3d1d21feb4ae5ef7d0a45f97e2456c017412
Marcel Andre Schneider <marcel.andre.schneider@cern.ch>
2019-02-26 17:32:17 +0100

    First support for ConcurrentME

--- DQMServices/Core/interface/DQMStore.h
+++ DQMServices/Core/interface/DQMStore.h
@@ -2,6 +2,7 @@
 #define DQMSERVICES_CORE_DQMSTORE_H
 
 #include "DQMServices/Core/interface/MonitorElement.h"
+#include "DQMServices/Core/interface/ConcurrentMonitorElement.h"
 
 #include <limits>
 
@@ -29,14 +30,14 @@ public:
     // ynbins is useless here, should be dropped.
     MonitorElement* bookProfile(std::string name, std::string title = "", 
         int xnbins = 30, double xlow = 0.0, double xhigh = 1.0,
-        int ynbins = 0, double ylow = -INF, double yhigh = +INF) { return nullptr; };
+        int ynbins = 0, double ylow = -INF, double yhigh = +INF, std::string opts = "") { return nullptr; };
     MonitorElement* bookProfile2D(std::string name, std::string title = "", 
         int xnbins = 30, double xlow = 0.0, double xhigh = 1.0,
         int ynbins = 30, double ylow = 0.0, double yhigh = 1.0,
         double zlow = -INF, double zhigh = +INF) { return nullptr; };
 
-    // TProfile takes options. Maybe get rid of that?
-    DQM_DEPRECATED MonitorElement* bookProfile(std::string name, std::string title = "", 
+    // TProfile takes options. 
+    MonitorElement* bookProfile(std::string name, std::string title = "", 
         int xnbins = 30, double xlow = 0.0, double xhigh = 1.0,
         double ylow = -INF, double yhigh = +INF, std::string opts = "") { return nullptr; };
     // a version taking bare buffers?
@@ -54,6 +55,36 @@ public:
     MonitorElement* bookProfile2D(std::string name, TProfile2D* object) { return nullptr; };
   };
 
+  class ConcurrentBooker {
+  public:
+    void setCurrentFolder(std::string folder) {};
+    DQM_DEPRECATED void cd() {};
+
+    ConcurrentMonitorElement bookInt(std::string name, std::string title = "") { return ConcurrentMonitorElement(); };
+    ConcurrentMonitorElement bookString(std::string name, std::string title = "") { return ConcurrentMonitorElement(); };
+
+   // TODO: replicating all the variations of arguments here requires more work
+   // This section catches many ambiguous numeric variations, likely incorrectly.
+    ConcurrentMonitorElement book1D(std::string name, std::string title = "", 
+        int xnbins = 30, double xlow = 0.0, double xhigh = 1.0) { return ConcurrentMonitorElement(); };
+    ConcurrentMonitorElement book2D(std::string name, std::string title = "", 
+        int xnbins = 30, double xlow = 0.0, double xhigh = 1.0,
+        int ynbins = 30, double ylow = 0.0, double yhigh = 1.0) { return ConcurrentMonitorElement(); };
+    // ynbins is useless here, should be dropped.
+    ConcurrentMonitorElement bookProfile(std::string name, std::string title = "", 
+        int xnbins = 30, double xlow = 0.0, double xhigh = 1.0,
+        int ynbins = 0, double ylow = -INF, double yhigh = +INF, std::string opts = "") { return ConcurrentMonitorElement(); };
+    ConcurrentMonitorElement bookProfile2D(std::string name, std::string title = "", 
+        int xnbins = 30, double xlow = 0.0, double xhigh = 1.0,
+        int ynbins = 30, double ylow = 0.0, double yhigh = 1.0,
+        int znbins = 0,  double zlow = -INF, double zhigh = +INF, std::string opts = "") { return ConcurrentMonitorElement(); };
+    ConcurrentMonitorElement book1D(std::string name, TH1* object) { return ConcurrentMonitorElement(); };
+    ConcurrentMonitorElement book2D(std::string name, TH2* object) { return ConcurrentMonitorElement(); };
+    ConcurrentMonitorElement book3D(std::string name, TH3* object) { return ConcurrentMonitorElement(); };
+    ConcurrentMonitorElement bookProfile(std::string name, TProfile* object) { return ConcurrentMonitorElement(); };
+    ConcurrentMonitorElement bookProfile2D(std::string name, TProfile2D* object) { return ConcurrentMonitorElement(); };
+  };
+
   class IGetter {
   public:
     MonitorElement* get(std::string name) { return nullptr; };
@@ -64,11 +95,25 @@ public:
   };
 
   DQM_DEPRECATED void save(std::string filename) {};
+  DQM_DEPRECATED void open(std::string filename, bool) {};
   DQM_DEPRECATED void setVerbose(int level) {};
   DQM_DEPRECATED void showDirStructure() {};
   DQM_DEPRECATED void setCurrentFolder(std::string folder) {};
   DQM_DEPRECATED MonitorElement* get(std::string name) { return nullptr; };
 
+
+  // legacy booking interface
+  DQM_DEPRECATED MonitorElement* book1D(std::string name, TH1* object) { return nullptr; };
+  DQM_DEPRECATED MonitorElement* book1S(std::string name, TH1* object) { return nullptr; };
+  DQM_DEPRECATED MonitorElement* book2D(std::string name, TH2* object) { return nullptr; };
+  DQM_DEPRECATED MonitorElement* book2S(std::string name, TH2* object) { return nullptr; };
+  DQM_DEPRECATED MonitorElement* book3D(std::string name, TH3* object) { return nullptr; };
+  DQM_DEPRECATED MonitorElement* bookProfile(std::string name, TProfile* object) { return nullptr; };
+  DQM_DEPRECATED MonitorElement* bookProfile2D(std::string name, TProfile2D* object) { return nullptr; };
+
+  DQM_DEPRECATED std::vector<std::string> getSubdirs() { return {}; };
+  DQM_DEPRECATED std::vector<std::string> getMEs() { return {}; };
+  DQM_DEPRECATED void rmdir(std::string) {};
 
   // APIs to be removed
   template <typename iFunc>
