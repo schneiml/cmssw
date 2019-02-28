c28c9cd60cbfff45e2e1c2bfbe7d663e51b8c348
Marcel Andre Schneider <marcel.andre.schneider@cern.ch>
2019-02-28 11:51:16 +0100

    Some more book-variations

--- DQMServices/Core/interface/DQMStore.h
+++ DQMServices/Core/interface/DQMStore.h
@@ -20,6 +20,7 @@ public:
     DQM_DEPRECATED std::string pwd() { return ""; };
 
     MonitorElement* bookInt(std::string name, std::string title = "") { return nullptr; };
+    MonitorElement* bookFloat(std::string name, std::string title = "") { return nullptr; };
     MonitorElement* bookString(std::string name, std::string title = "") { return nullptr; };
 
    // TODO: replicating all the variations of arguments here requires more work
@@ -27,6 +28,16 @@ public:
     MonitorElement* book1D(std::string name, std::string title = "", 
         int xnbins = 30, double xlow = 0.0, double xhigh = 1.0) { return nullptr; };
     MonitorElement* book2D(std::string name, std::string title = "", 
+        int xnbins = 30, double xlow = 0.0, double xhigh = 1.0,
+        int ynbins = 30, double ylow = 0.0, double yhigh = 1.0) { return nullptr; };
+    MonitorElement* book1S(std::string name, std::string title = "", // "S" is a 16bit int version 
+        int xnbins = 30, double xlow = 0.0, double xhigh = 1.0) { return nullptr; };
+    MonitorElement* book2S(std::string name, std::string title = "",
+        int xnbins = 30, double xlow = 0.0, double xhigh = 1.0,
+        int ynbins = 30, double ylow = 0.0, double yhigh = 1.0) { return nullptr; };
+    MonitorElement* book1DD(std::string name, std::string title = "",  // "DD" are double versions, TH1F vs. TH1D
+        int xnbins = 30, double xlow = 0.0, double xhigh = 1.0) { return nullptr; };
+    MonitorElement* book2DD(std::string name, std::string title = "", 
         int xnbins = 30, double xlow = 0.0, double xhigh = 1.0,
         int ynbins = 30, double ylow = 0.0, double yhigh = 1.0) { return nullptr; };
     // ynbins is useless here, should be dropped.
@@ -42,15 +53,30 @@ public:
     MonitorElement* bookProfile(std::string name, std::string title = "", 
         int xnbins = 30, double xlow = 0.0, double xhigh = 1.0,
         double ylow = -INF, double yhigh = +INF, std::string opts = "") { return nullptr; };
+    MonitorElement* bookProfile2D(std::string name, std::string title = "", 
+        int xnbins = 30, double xlow = 0.0, double xhigh = 1.0,
+        int ynbins = 30, double ylow = 0.0, double yhigh = 1.0,
+        int znbins = 0, double zlow = -INF, double zhigh = +INF, std::string opts = "") { return nullptr; };
     // a version taking bare buffers?
     DQM_DEPRECATED MonitorElement* book1D(std::string name, std::string title, 
         int xnbins, const float* buf) { return nullptr; };
     DQM_DEPRECATED MonitorElement* bookProfile(std::string name, std::string title, 
         int xnbins, const double* buf,
         int ynbbins, double ylow, double yhigh) { return nullptr; };
-    DQM_DEPRECATED MonitorElement* bookProfile(std::string name, std::string title = "", 
-        int xnbins = 30, double* buf = nullptr, // ???
-        double ylow = -INF, double yhigh = +INF, std::string opts = "") { return nullptr; };
+    DQM_DEPRECATED MonitorElement* book2D(std::string name, std::string title, 
+        int xnbins, float* xbins,
+        int ynbins, float* ybins) { return nullptr; };
+    DQM_DEPRECATED MonitorElement* bookProfile(std::string name, std::string title, 
+        int xnbins, double* buf, // ???
+        double ylow, double yhigh, std::string opts = "") { return nullptr; };
+
+    // versions taking TString, since there is no automatic conversion
+    DQM_DEPRECATED MonitorElement* book1D(TString const& name, TString const& title = "", 
+        int xnbins = 30, double xlow = 0.0, double xhigh = 1.0) { return nullptr; };
+    DQM_DEPRECATED MonitorElement* book2D(TString const& name, TString const& title = "", 
+        int xnbins = 30, double xlow = 0.0, double xhigh = 1.0,
+        int ynbins = 30, double ylow = 0.0, double yhigh = 1.0) { return nullptr; };
+
 
     // A version of each taking a existing ROOT object.
     MonitorElement* book1D(std::string name, TH1* object) { return nullptr; };
@@ -75,7 +101,12 @@ public:
    // This section catches many ambiguous numeric variations, likely incorrectly.
     ConcurrentMonitorElement book1D(std::string name, std::string title = "", 
         int xnbins = 30, double xlow = 0.0, double xhigh = 1.0) { return ConcurrentMonitorElement(); };
+    ConcurrentMonitorElement book1DD(std::string name, std::string title = "", 
+        int xnbins = 30, double xlow = 0.0, double xhigh = 1.0) { return ConcurrentMonitorElement(); };
     ConcurrentMonitorElement book2D(std::string name, std::string title = "", 
+        int xnbins = 30, double xlow = 0.0, double xhigh = 1.0,
+        int ynbins = 30, double ylow = 0.0, double yhigh = 1.0) { return ConcurrentMonitorElement(); };
+    ConcurrentMonitorElement book2S(std::string name, std::string title = "", // 16bit int version
         int xnbins = 30, double xlow = 0.0, double xhigh = 1.0,
         int ynbins = 30, double ylow = 0.0, double yhigh = 1.0) { return ConcurrentMonitorElement(); };
     // ynbins is useless here, should be dropped.
@@ -91,6 +122,10 @@ public:
     ConcurrentMonitorElement book3D(std::string name, TH3* object) { return ConcurrentMonitorElement(); };
     ConcurrentMonitorElement bookProfile(std::string name, TProfile* object) { return ConcurrentMonitorElement(); };
     ConcurrentMonitorElement bookProfile2D(std::string name, TProfile2D* object) { return ConcurrentMonitorElement(); };
+
+    DQM_DEPRECATED MonitorElement* book2D(std::string name, std::string title, 
+        int xnbins, float* xbins,
+        int ynbins, float* ybins) { return nullptr; };
   };
 
   class IGetter {
@@ -110,7 +145,7 @@ public:
   };
 
   DQM_DEPRECATED void save(std::string filename) {};
-  DQM_DEPRECATED void open(std::string filename, bool) {};
+  DQM_DEPRECATED void open(std::string filename, bool something = false) {};
   DQM_DEPRECATED void setVerbose(int level) {};
   DQM_DEPRECATED void showDirStructure() {};
   DQM_DEPRECATED void setCurrentFolder(std::string folder) {};
@@ -120,18 +155,30 @@ public:
   // legacy booking interface
   DQM_DEPRECATED void cd(std::string folder = "") {};
   DQM_DEPRECATED std::string pwd() { return ""; };
+  DQM_DEPRECATED MonitorElement* bookInt(std::string name, std::string title = "") { return nullptr; };
+  DQM_DEPRECATED MonitorElement* bookFloat(std::string name, std::string title = "") { return nullptr; };
+  DQM_DEPRECATED MonitorElement* bookString(std::string name, std::string title = "") { return nullptr; };
   DQM_DEPRECATED MonitorElement* book1D(std::string name, TH1* object) { return nullptr; };
+  DQM_DEPRECATED MonitorElement* book1DD(std::string name, TH1* object) { return nullptr; };
   DQM_DEPRECATED MonitorElement* book1S(std::string name, TH1* object) { return nullptr; };
   DQM_DEPRECATED MonitorElement* book2D(std::string name, TH2* object) { return nullptr; };
+  DQM_DEPRECATED MonitorElement* book2DD(std::string name, TH2* object) { return nullptr; };
   DQM_DEPRECATED MonitorElement* book2S(std::string name, TH2* object) { return nullptr; };
   DQM_DEPRECATED MonitorElement* book3D(std::string name, TH3* object) { return nullptr; };
   DQM_DEPRECATED MonitorElement* bookProfile(std::string name, TProfile* object) { return nullptr; };
   DQM_DEPRECATED MonitorElement* bookProfile2D(std::string name, TProfile2D* object) { return nullptr; };
   DQM_DEPRECATED MonitorElement* book1D(std::string name, std::string title = "", 
     int xnbins = 30, double xlow = 0.0, double xhigh = 1.0) { return nullptr; };
+  DQM_DEPRECATED MonitorElement* book1DD(std::string name, std::string title = "", 
+    int xnbins = 30, double xlow = 0.0, double xhigh = 1.0) { return nullptr; };
   DQM_DEPRECATED MonitorElement* book2D(std::string name, std::string title = "", 
     int xnbins = 30, double xlow = 0.0, double xhigh = 1.0,
     int ynbins = 30, double ylow = 0.0, double yhigh = 1.0) { return nullptr; };
+  DQM_DEPRECATED MonitorElement* book2DD(std::string name, std::string title = "", 
+    int xnbins = 30, double xlow = 0.0, double xhigh = 1.0,
+    int ynbins = 30, double ylow = 0.0, double yhigh = 1.0) { return nullptr; };
+  DQM_DEPRECATED MonitorElement* book1D(std::string name, std::string title, 
+      int xnbins, const float* buf) { return nullptr; };
 
   DQM_DEPRECATED std::vector<std::string> getSubdirs() { return {}; };
   DQM_DEPRECATED std::vector<std::string> getMEs() { return {}; };
