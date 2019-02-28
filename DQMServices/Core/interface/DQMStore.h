f600362e4de30780128192318af97bde6228a168
Marcel Andre Schneider <marcel.andre.schneider@cern.ch>
2019-02-28 16:22:08 +0100

    Try to systematically define the booking interface

--- DQMServices/Core/interface/DQMStore.h
+++ DQMServices/Core/interface/DQMStore.h
@@ -24,67 +24,40 @@ public:
     MonitorElement* bookFloat(std::string name, std::string title = "") { return nullptr; };
     MonitorElement* bookString(std::string name, std::string title = "") { return nullptr; };
 
-   // TODO: replicating all the variations of arguments here requires more work
-   // This section catches many ambiguous numeric variations, likely incorrectly.
-    MonitorElement* book1D(const char*  name, const char*  title = "",  // char* here so it does not conflict with the TString version
-        int xnbins = 30, double xlow = 0.0, double xhigh = 1.0) { return nullptr; };
-    MonitorElement* book2D(const char*  name, const char*  title = "", 
-        int xnbins = 30, double xlow = 0.0, double xhigh = 1.0,
-        int ynbins = 30, double ylow = 0.0, double yhigh = 1.0) { return nullptr; };
-    MonitorElement* book1S(std::string name, std::string title = "", // "S" is a 16bit int version 
-        int xnbins = 30, double xlow = 0.0, double xhigh = 1.0) { return nullptr; };
-    MonitorElement* book2S(std::string name, std::string title = "",
-        int xnbins = 30, double xlow = 0.0, double xhigh = 1.0,
-        int ynbins = 30, double ylow = 0.0, double yhigh = 1.0) { return nullptr; };
-    MonitorElement* book1DD(std::string name, std::string title = "",  // "DD" are double versions, TH1F vs. TH1D
-        int xnbins = 30, double xlow = 0.0, double xhigh = 1.0) { return nullptr; };
-    MonitorElement* book2DD(std::string name, std::string title = "", 
-        int xnbins = 30, double xlow = 0.0, double xhigh = 1.0,
-        int ynbins = 30, double ylow = 0.0, double yhigh = 1.0) { return nullptr; };
-    // ynbins is useless here, should be dropped.
-    MonitorElement* bookProfile(std::string name, std::string title = "", 
-        int xnbins = 30, double xlow = 0.0, double xhigh = 1.0,
-        int ynbins = 0, double ylow = -INF, double yhigh = +INF, std::string opts = "") { return nullptr; };
-    MonitorElement* bookProfile(const char* name, const char* title = "", 
-        int xnbins = 30, double xlow = 0.0, double xhigh = 1.0,
-        double ylow = -INF, double yhigh = +INF, std::string opts = "") { return nullptr; };
-    MonitorElement* bookProfile2D(std::string name, std::string title = "", 
-        int xnbins = 30, double xlow = 0.0, double xhigh = 1.0,
-        int ynbins = 30, double ylow = 0.0, double yhigh = 1.0,
-        double zlow = -INF, double zhigh = +INF, std::string opts = "") { return nullptr; };
-    MonitorElement* bookProfile2D(const char* name, const char* title,
-        int xnbins, double xlow, double xhigh,
-        int ynbins, double ylow, double yhigh,
-        int znbins, double zlow, double zhigh, std::string opts = "") { return nullptr; };
+#define NAMETITLE TString const& name, TString const& title
+#define XBINRANGE int xnbins, double xlow, double xhigh
+#define YBINRANGE int ynbins, double ylow, double yhigh
+#define ZBINRANGE int znbins, double zlow, double zhigh
+#define XBINVALUES int xnbins, float* xbinvalues
+#define YBINVALUES int ynbins, float* ybinvalues
+#define YRANGE double ylow, double yhigh
+#define ZRANGE double zlow, double zhigh
+    MonitorElement* book1D(NAMETITLE, XBINRANGE) { return nullptr; };
+    MonitorElement* book1D(NAMETITLE, XBINVALUES) { return nullptr; };
+    MonitorElement* book2D(NAMETITLE, XBINRANGE, YBINRANGE) { return nullptr; };
+    MonitorElement* book2D(NAMETITLE, XBINVALUES, YBINVALUES) { return nullptr; };
+    MonitorElement* book3D(NAMETITLE, XBINRANGE, YBINRANGE, ZBINRANGE) { return nullptr; };
+    MonitorElement* book1DD(NAMETITLE, XBINRANGE) { return nullptr; };
+    MonitorElement* book1DD(NAMETITLE, XBINVALUES) { return nullptr; };
+    MonitorElement* book2DD(NAMETITLE, XBINRANGE, YBINRANGE) { return nullptr; };
+    MonitorElement* book2DD(NAMETITLE, XBINVALUES, YBINVALUES) { return nullptr; };
+    MonitorElement* book1S(NAMETITLE, XBINRANGE) { return nullptr; };
+    MonitorElement* book1S(NAMETITLE, XBINVALUES) { return nullptr; };
+    MonitorElement* book2S(NAMETITLE, XBINRANGE, YBINRANGE) { return nullptr; };
+    MonitorElement* book2S(NAMETITLE, XBINVALUES, YBINVALUES) { return nullptr; };
 
-    // a version taking bare buffers?
-    DQM_DEPRECATED MonitorElement* book1D(std::string name, std::string title, 
-        int xnbins, const float* buf) { return nullptr; };
-    DQM_DEPRECATED MonitorElement* bookProfile(std::string name, std::string title, 
-        int xnbins, const double* buf,
-        int ynbbins, double ylow, double yhigh) { return nullptr; };
-    DQM_DEPRECATED MonitorElement* book2D(std::string name, std::string title, 
-        int xnbins, float* xbins,
-        int ynbins, float* ybins) { return nullptr; };
-    DQM_DEPRECATED MonitorElement* bookProfile(std::string name, std::string title, 
-        int xnbins, double* buf, // ???
-        double ylow, double yhigh, std::string opts = "") { return nullptr; };
+    MonitorElement* bookProfile(NAMETITLE, XBINRANGE, YRANGE, const char* opts = "") { return nullptr; };
+    MonitorElement* bookProfile(NAMETITLE, XBINRANGE, YBINRANGE, const char* opts = "") { return nullptr; };
+    MonitorElement* bookProfile2D(NAMETITLE, XBINRANGE, YBINRANGE, ZRANGE, const char* opts = "") { return nullptr; };
+    MonitorElement* bookProfile2D(NAMETITLE, XBINRANGE, YBINRANGE, ZBINRANGE, const char* opts = "") { return nullptr; };
 
-    // versions taking TString, since there is no automatic conversion
-    DQM_DEPRECATED MonitorElement* book1D(TString const& name, TString const& title = "", 
-        int xnbins = 30, double xlow = 0.0, double xhigh = 1.0) { return nullptr; };
-    DQM_DEPRECATED MonitorElement* book2D(TString const& name, TString const& title = "", 
-        int xnbins = 30, double xlow = 0.0, double xhigh = 1.0,
-        int ynbins = 30, double ylow = 0.0, double yhigh = 1.0) { return nullptr; };
-    DQM_DEPRECATED MonitorElement* bookProfile(TString const& name, TString const& title = "", 
-        int xnbins = 30, double xlow = 0.0, double xhigh = 1.0,
-        double ylow = -INF, double yhigh = +INF, std::string opts = "") { return nullptr; };
-
-
-    // A version of each taking a existing ROOT object.
     MonitorElement* book1D(std::string name, TH1* object) { return nullptr; };
     MonitorElement* book2D(std::string name, TH2* object) { return nullptr; };
     MonitorElement* book3D(std::string name, TH3* object) { return nullptr; };
+    MonitorElement* book1DD(std::string name, TH1* object) { return nullptr; };
+    MonitorElement* book2DD(std::string name, TH2* object) { return nullptr; };
+    MonitorElement* book1S(std::string name, TH1* object) { return nullptr; };
+    MonitorElement* book2S(std::string name, TH2* object) { return nullptr; };
     MonitorElement* bookProfile(std::string name, TProfile* object) { return nullptr; };
     MonitorElement* bookProfile2D(std::string name, TProfile2D* object) { return nullptr; };
 
@@ -98,37 +71,38 @@ public:
     DQM_DEPRECATED void cd() {};
 
     ConcurrentMonitorElement bookInt(std::string name, std::string title = "") { return ConcurrentMonitorElement(); };
+    ConcurrentMonitorElement bookFloat(std::string name, std::string title = "") { return ConcurrentMonitorElement(); };
     ConcurrentMonitorElement bookString(std::string name, std::string title = "") { return ConcurrentMonitorElement(); };
 
-   // TODO: replicating all the variations of arguments here requires more work
-   // This section catches many ambiguous numeric variations, likely incorrectly.
-    ConcurrentMonitorElement book1D(std::string name, std::string title = "", 
-        int xnbins = 30, double xlow = 0.0, double xhigh = 1.0) { return ConcurrentMonitorElement(); };
-    ConcurrentMonitorElement book1DD(std::string name, std::string title = "", 
-        int xnbins = 30, double xlow = 0.0, double xhigh = 1.0) { return ConcurrentMonitorElement(); };
-    ConcurrentMonitorElement book2D(std::string name, std::string title = "", 
-        int xnbins = 30, double xlow = 0.0, double xhigh = 1.0,
-        int ynbins = 30, double ylow = 0.0, double yhigh = 1.0) { return ConcurrentMonitorElement(); };
-    ConcurrentMonitorElement book2S(std::string name, std::string title = "", // 16bit int version
-        int xnbins = 30, double xlow = 0.0, double xhigh = 1.0,
-        int ynbins = 30, double ylow = 0.0, double yhigh = 1.0) { return ConcurrentMonitorElement(); };
-    // ynbins is useless here, should be dropped.
-    ConcurrentMonitorElement bookProfile(std::string name, std::string title = "", 
-        int xnbins = 30, double xlow = 0.0, double xhigh = 1.0,
-        int ynbins = 0, double ylow = -INF, double yhigh = +INF, std::string opts = "") { return ConcurrentMonitorElement(); };
-    ConcurrentMonitorElement bookProfile2D(std::string name, std::string title = "", 
-        int xnbins = 30, double xlow = 0.0, double xhigh = 1.0,
-        int ynbins = 30, double ylow = 0.0, double yhigh = 1.0,
-        int znbins = 0,  double zlow = -INF, double zhigh = +INF, std::string opts = "") { return ConcurrentMonitorElement(); };
+    ConcurrentMonitorElement book1D(NAMETITLE, XBINRANGE) { return ConcurrentMonitorElement(); };
+    ConcurrentMonitorElement book1D(NAMETITLE, XBINVALUES) { return ConcurrentMonitorElement(); };
+    ConcurrentMonitorElement book2D(NAMETITLE, XBINRANGE, YBINRANGE) { return ConcurrentMonitorElement(); };
+    ConcurrentMonitorElement book2D(NAMETITLE, XBINVALUES, YBINVALUES) { return ConcurrentMonitorElement(); };
+    ConcurrentMonitorElement book3D(NAMETITLE, XBINRANGE, YBINRANGE, ZBINRANGE) { return ConcurrentMonitorElement(); };
+    ConcurrentMonitorElement book1DD(NAMETITLE, XBINRANGE) { return ConcurrentMonitorElement(); };
+    ConcurrentMonitorElement book1DD(NAMETITLE, XBINVALUES) { return ConcurrentMonitorElement(); };
+    ConcurrentMonitorElement book2DD(NAMETITLE, XBINRANGE, YBINRANGE) { return ConcurrentMonitorElement(); };
+    ConcurrentMonitorElement book2DD(NAMETITLE, XBINVALUES, YBINVALUES) { return ConcurrentMonitorElement(); };
+    ConcurrentMonitorElement book1S(NAMETITLE, XBINRANGE) { return ConcurrentMonitorElement(); };
+    ConcurrentMonitorElement book1S(NAMETITLE, XBINVALUES) { return ConcurrentMonitorElement(); };
+    ConcurrentMonitorElement book2S(NAMETITLE, XBINRANGE, YBINRANGE) { return ConcurrentMonitorElement(); };
+    ConcurrentMonitorElement book2S(NAMETITLE, XBINVALUES, YBINVALUES) { return ConcurrentMonitorElement(); };
+
+    ConcurrentMonitorElement bookProfile(NAMETITLE, XBINRANGE, YRANGE, const char* opts = "") { return ConcurrentMonitorElement(); };
+    ConcurrentMonitorElement bookProfile(NAMETITLE, XBINRANGE, YBINRANGE, const char* opts = "") { return ConcurrentMonitorElement(); };
+    ConcurrentMonitorElement bookProfile2D(NAMETITLE, XBINRANGE, YBINRANGE, ZRANGE, const char* opts = "") { return ConcurrentMonitorElement(); };
+    ConcurrentMonitorElement bookProfile2D(NAMETITLE, XBINRANGE, YBINRANGE, ZBINRANGE, const char* opts = "") { return ConcurrentMonitorElement(); };
+
     ConcurrentMonitorElement book1D(std::string name, TH1* object) { return ConcurrentMonitorElement(); };
     ConcurrentMonitorElement book2D(std::string name, TH2* object) { return ConcurrentMonitorElement(); };
     ConcurrentMonitorElement book3D(std::string name, TH3* object) { return ConcurrentMonitorElement(); };
+    ConcurrentMonitorElement book1DD(std::string name, TH1* object) { return ConcurrentMonitorElement(); };
+    ConcurrentMonitorElement book2DD(std::string name, TH2* object) { return ConcurrentMonitorElement(); };
+    ConcurrentMonitorElement book1S(std::string name, TH1* object) { return ConcurrentMonitorElement(); };
+    ConcurrentMonitorElement book2S(std::string name, TH2* object) { return ConcurrentMonitorElement(); };
     ConcurrentMonitorElement bookProfile(std::string name, TProfile* object) { return ConcurrentMonitorElement(); };
     ConcurrentMonitorElement bookProfile2D(std::string name, TProfile2D* object) { return ConcurrentMonitorElement(); };
 
-    DQM_DEPRECATED MonitorElement* book2D(std::string name, std::string title, 
-        int xnbins, float* xbins,
-        int ynbins, float* ybins) { return nullptr; };
   };
 
   class IGetter {
@@ -155,44 +129,52 @@ public:
   DQM_DEPRECATED void open(std::string filename, bool something = false, std::string a = "", std::string b = "", OpenRunDirs mode = KeepRunDirs) {}; // last args for reference?
   DQM_DEPRECATED void setVerbose(int level) {};
   DQM_DEPRECATED void showDirStructure() {};
-  DQM_DEPRECATED void setCurrentFolder(std::string folder) {};
   DQM_DEPRECATED MonitorElement* get(std::string name) { return nullptr; };
 
 
   // legacy booking interface
+  DQM_DEPRECATED void setCurrentFolder(std::string folder) {};
   DQM_DEPRECATED void cd(std::string folder = "") {};
+  DQM_DEPRECATED void goUp() {};
   DQM_DEPRECATED std::string pwd() { return ""; };
+
   DQM_DEPRECATED MonitorElement* bookInt(std::string name, std::string title = "") { return nullptr; };
   DQM_DEPRECATED MonitorElement* bookFloat(std::string name, std::string title = "") { return nullptr; };
   DQM_DEPRECATED MonitorElement* bookString(std::string name, std::string title = "") { return nullptr; };
+
+  DQM_DEPRECATED MonitorElement* book1D(NAMETITLE, XBINRANGE) { return nullptr; };
+  DQM_DEPRECATED MonitorElement* book1D(NAMETITLE, XBINVALUES) { return nullptr; };
+  DQM_DEPRECATED MonitorElement* book2D(NAMETITLE, XBINRANGE, YBINRANGE) { return nullptr; };
+  DQM_DEPRECATED MonitorElement* book2D(NAMETITLE, XBINVALUES, YBINVALUES) { return nullptr; };
+  DQM_DEPRECATED MonitorElement* book3D(NAMETITLE, XBINRANGE, YBINRANGE, ZBINRANGE) { return nullptr; };
+  DQM_DEPRECATED MonitorElement* book1DD(NAMETITLE, XBINRANGE) { return nullptr; };
+  DQM_DEPRECATED MonitorElement* book1DD(NAMETITLE, XBINVALUES) { return nullptr; };
+  DQM_DEPRECATED MonitorElement* book2DD(NAMETITLE, XBINRANGE, YBINRANGE) { return nullptr; };
+  DQM_DEPRECATED MonitorElement* book2DD(NAMETITLE, XBINVALUES, YBINVALUES) { return nullptr; };
+  DQM_DEPRECATED MonitorElement* book1S(NAMETITLE, XBINRANGE) { return nullptr; };
+  DQM_DEPRECATED MonitorElement* book1S(NAMETITLE, XBINVALUES) { return nullptr; };
+  DQM_DEPRECATED MonitorElement* book2S(NAMETITLE, XBINRANGE, YBINRANGE) { return nullptr; };
+  DQM_DEPRECATED MonitorElement* book2S(NAMETITLE, XBINVALUES, YBINVALUES) { return nullptr; };
+
+  DQM_DEPRECATED MonitorElement* bookProfile(NAMETITLE, XBINRANGE, YRANGE, const char* opts = "") { return nullptr; };
+  DQM_DEPRECATED MonitorElement* bookProfile(NAMETITLE, XBINRANGE, YBINRANGE, const char* opts = "") { return nullptr; };
+  DQM_DEPRECATED MonitorElement* bookProfile2D(NAMETITLE, XBINRANGE, YBINRANGE, ZRANGE, const char* opts = "") { return nullptr; };
+  DQM_DEPRECATED MonitorElement* bookProfile2D(NAMETITLE, XBINRANGE, YBINRANGE, ZBINRANGE, const char* opts = "") { return nullptr; };
+
   DQM_DEPRECATED MonitorElement* book1D(std::string name, TH1* object) { return nullptr; };
-  DQM_DEPRECATED MonitorElement* book1DD(std::string name, TH1* object) { return nullptr; };
-  DQM_DEPRECATED MonitorElement* book1S(std::string name, TH1* object) { return nullptr; };
   DQM_DEPRECATED MonitorElement* book2D(std::string name, TH2* object) { return nullptr; };
-  DQM_DEPRECATED MonitorElement* book2DD(std::string name, TH2* object) { return nullptr; };
-  DQM_DEPRECATED MonitorElement* book2S(std::string name, TH2* object) { return nullptr; };
   DQM_DEPRECATED MonitorElement* book3D(std::string name, TH3* object) { return nullptr; };
+  DQM_DEPRECATED MonitorElement* book1DD(std::string name, TH1* object) { return nullptr; };
+  DQM_DEPRECATED MonitorElement* book2DD(std::string name, TH2* object) { return nullptr; };
+  DQM_DEPRECATED MonitorElement* book1S(std::string name, TH1* object) { return nullptr; };
+  DQM_DEPRECATED MonitorElement* book2S(std::string name, TH2* object) { return nullptr; };
   DQM_DEPRECATED MonitorElement* bookProfile(std::string name, TProfile* object) { return nullptr; };
   DQM_DEPRECATED MonitorElement* bookProfile2D(std::string name, TProfile2D* object) { return nullptr; };
-  DQM_DEPRECATED MonitorElement* book1D(std::string name, std::string title = "", 
-    int xnbins = 30, double xlow = 0.0, double xhigh = 1.0) { return nullptr; };
-  DQM_DEPRECATED MonitorElement* book1D(std::string name, std::string title, 
-    int xnbins, const float* buf) { return nullptr; };
-  DQM_DEPRECATED MonitorElement* book1DD(std::string name, std::string title = "", 
-    int xnbins = 30, double xlow = 0.0, double xhigh = 1.0) { return nullptr; };
-  DQM_DEPRECATED MonitorElement* book2D(std::string name, std::string title = "", 
-    int xnbins = 30, double xlow = 0.0, double xhigh = 1.0,
-    int ynbins = 30, double ylow = 0.0, double yhigh = 1.0) { return nullptr; };
-  DQM_DEPRECATED MonitorElement* book2DD(std::string name, std::string title = "", 
-    int xnbins = 30, double xlow = 0.0, double xhigh = 1.0,
-    int ynbins = 30, double ylow = 0.0, double yhigh = 1.0) { return nullptr; };
-  DQM_DEPRECATED MonitorElement* bookProfile(std::string name, std::string title = "", 
-    int xnbins = 30, double xlow = 0.0, double xhigh = 1.0,
-    int ynbins = 0, double ylow = -INF, double yhigh = +INF, std::string opts = "") { return nullptr; };
-  DQM_DEPRECATED MonitorElement* book2S(std::string name, std::string title = "",
-        int xnbins = 30, double xlow = 0.0, double xhigh = 1.0,
-        int ynbins = 30, double ylow = 0.0, double yhigh = 1.0) { return nullptr; };
 
+  DQM_DEPRECATED void tag(MonitorElement* me, unsigned int tag);
+  DQM_DEPRECATED void tagContents(std::string, unsigned int tag);
+
+  // legacy getter interface
   DQM_DEPRECATED bool dirExists(std::string name) { return false; };
   DQM_DEPRECATED std::vector<std::string> getSubdirs() { return {}; };
   DQM_DEPRECATED std::vector<std::string> getMEs() { return {}; };
