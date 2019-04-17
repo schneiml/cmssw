a08ceeb258b2b2f4793400f28d50ba6d7a1cd605
Marcel Andre Schneider <marcel.andre.schneider@cern.ch>
2019-04-17 14:39:07 +0200

    More advanced conversion

--- DQMServices/Components/scripts/csv_to_sqlite.sh
+++ DQMServices/Components/scripts/csv_to_sqlite.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
 # Convert the CSV format output by the DXR indexer plugin into a SQLlite database.
-# The CSV contains key-value data, with keys in the even and values in the odd columns,
-# the first column is the type of the reference.
-# This script generates and runs some SQL statements to transform the data into one table of names columns.
 
-# call as csv_to_sqlite.sh <all.csv>, where all.csv is the concatenation of all the csv files produced.
+report () {
+  echo "$@" 1>&2
+  "$@"
+}
 
-DB="sqlite3 ${1}.sqlite"
+find . -maxdepth 1 -name '*.csv' | xargs cat > allcsv
 
+rm -f dxr.sqlite
+DB="sqlite3 dxr.sqlite"
+
+# First, import the CSV file. 
 $DB 2> /dev/null <<EOF
 CREATE TABLE dxrdata(thing, k1, v1, k2, v2, k3, v3, k4, v4, k5, v5, k6, v6, k7, v7, k8, v8);
 .mode csv
-.import $1 dxrdata
+.import allcsv dxrdata
 EOF
 
+# The CSV contains key-value data, with keys in the even and values in the odd columns,
+# the first column is the type of the reference.
+# This script generates and runs some SQL statements to transform the data into one table of names columns.
 columns=$( ($DB 2> /dev/null ; echo dummy) <<EOF
 SELECT DISTINCT k1 || "," FROM (
         SELECT DISTINCT k1 FROM dxrdata 
@@ -30,13 +37,14 @@ SELECT DISTINCT k1 || "," FROM (
 EOF
 )
 
+# Then, make a table with these columns.
 $DB <<EOF
 CREATE TABLE dxrcols(thing,
 $columns
 );
 EOF
 
-
+# Finally, make statements that transform the data
 inserts=$($DB <<EOF
 SELECT DISTINCT "INSERT INTO dxrcols(thing" ||
   CASE WHEN k1 IS NOT NULL THEN "," || k1 ELSE "" END ||
@@ -69,7 +77,65 @@ SELECT DISTINCT "INSERT INTO dxrcols(thing" ||
 EOF
 )
 
+# and execute them.
 $DB <<EOF
 $inserts
+CREATE INDEX things on dxrcols(thing);
 DROP TABLE dxrdata;
 EOF
+
+# Now, we can split the big table into multiple small ones.
+# Make one per type of row, check with a qurery for each column if there are 
+# any non-null values, and cut column numbers from the loc values.
+# Also add an index for each loc column for future use.
+things=$($DB <<EOF
+SELECT DISTINCT thing FROM dxrcols;
+EOF
+)
+for t in $things; do 
+  cols=""
+  selects=""
+  index=""
+  for c in $columns; do
+    used=$( $DB <<EOF
+    SELECT max(${c%,}) FROM dxrcols WHERE thing="$t";
+EOF
+    )
+    if [ ! -z "$used" ]; then
+      echo $c used in $t >&2
+      cols="$cols $c"
+      if [[ $c == *loc, ]]; then
+        echo "  " $c is loc >&2
+        selects="$selects rtrim(rtrim(${c%,}, '0123456789'), ':'),"
+        index="$index CREATE INDEX ${t}_${c%,} ON $t(${c%,});"
+      else
+        selects="$selects $c"
+      fi
+
+    fi
+  done
+  echo "CREATE TABLE $t($cols dummy);"
+  echo "$index"
+  echo "INSERT INTO $t SELECT $selects dummy FROM dxrcols WHERE thing='$t';" 
+done > transform
+$DB < transform
+
+# Add source code into the DB.
+$DB <<EOF
+CREATE TABLE code(loc, line);
+CREATE INDEX codeloc ON code(loc);
+EOF
+
+for f in $($DB <<EOF
+  SELECT DISTINCT rtrim(loc, '0123456789:') FROM ref;
+EOF
+); do
+  echo "Importing $f ..."
+  $DB <<EOF
+CREATE TEMP TABLE codefile(line);
+-- some very rare character, we don't want more than one column
+.separator "\v"
+.import $f codefile
+INSERT INTO code SELECT "$f:" || rowid as loc, line FROM codefile;
+EOF
+done
