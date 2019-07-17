7d352deeef9b497de370d8a99882b50cc18560a2
Marcel Andre Schneider <marcel.andre.schneider@cern.ch>
2019-07-17 18:34:12 +0200

    Run concurrentme-to-normal-me
    Cherry-pick: The tool was run on a 'clean' release, then this cmmit
    cherry-picked.

--- HLTrigger/Timer/plugins/ThroughputService.h
+++ HLTrigger/Timer/plugins/ThroughputService.h
@@ -44,8 +44,8 @@ public:
   static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
 
 private:
-  ConcurrentMonitorElement m_sourced_events;
-  ConcurrentMonitorElement m_retired_events;
+  dqm::reco::MonitorElement const* m_sourced_events;
+  dqm::reco::MonitorElement const* m_retired_events;
 
   std::chrono::steady_clock::time_point m_startup;
 
