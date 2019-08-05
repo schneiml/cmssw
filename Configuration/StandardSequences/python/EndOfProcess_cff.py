92db286b6b1f366f8b4b07578143175275cac52e
Marcel Andre Schneider <marcel.andre.schneider@cern.ch>
2019-08-05 17:45:41 +0200

    Remove MEtoEDM from default sequences.

--- Configuration/StandardSequences/python/EndOfProcess_cff.py
+++ Configuration/StandardSequences/python/EndOfProcess_cff.py
@@ -1,10 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
-#sometimes the dqm service is not already setup...
-DQMStore = cms.Service("DQMStore")
+endOfProcess=cms.Sequence()
 
-from DQMServices.Components.MEtoEDMConverter_cfi import *
-endOfProcess=cms.Sequence(MEtoEDMConverter)
-
-from DQMServices.Components.MEtoMEComparitor_cfi import *
-endOfProcess_withComparison = cms.Sequence(MEtoEDMConverter+MEtoMEComparitor)
+endOfProcess_withComparison = cms.Sequence()
