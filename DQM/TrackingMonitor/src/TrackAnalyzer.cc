eef576d60ef59ebc4f6a703e1f53173c3444d111
Marcel Andre Schneider <marcel.andre.schneider@cern.ch>
2019-12-13 11:07:28 +0100

    Remove setLumiFlag.
    And migrate all the usages.

--- DQM/TrackingMonitor/src/TrackAnalyzer.cc
+++ DQM/TrackingMonitor/src/TrackAnalyzer.cc
@@ -2434,16 +2434,6 @@ void TrackAnalyzer::fillHistosForTrackerSpecific(const reco::Track& track) {
   }
 }
 //
-// -- Set Lumi Flag
-//
-void TrackAnalyzer::setLumiFlag() {
-  TkParameterMEs tkmes;
-  if (Chi2oNDF_lumiFlag)
-    Chi2oNDF_lumiFlag->setLumiFlag();
-  if (NumberOfRecHitsPerTrack_lumiFlag)
-    NumberOfRecHitsPerTrack_lumiFlag->setLumiFlag();
-}
-//
 // -- Apply Reset
 //
 void TrackAnalyzer::doReset() {
