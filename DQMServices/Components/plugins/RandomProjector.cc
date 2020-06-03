232a7e4e2e252ebe0d881f04c07383dd63de88c3
Marcel Andre Schneider <marcel.andre.schneider@cern.ch>
2020-06-03 12:25:42 +0200

    Try a faster version of the random projection.

--- DQMServices/Components/plugins/RandomProjector.cc
+++ DQMServices/Components/plugins/RandomProjector.cc
@@ -44,8 +44,6 @@ private:
   std::string meprefix;
   std::string outputdir;
   bool writetostdout;
-
-  std::map<std::string, std::vector<double>> weights;
 };
 
 RandomProjector::RandomProjector(const edm::ParameterSet& ps) : DQMEDHarvester(ps) {
@@ -69,6 +67,120 @@ void RandomProjector::dqmEndJob(DQMStore::IBooker& ibooker, DQMStore::IGetter& i
   //performProjection(igetter, ibooker, MonitorElementData::Scope::JOB);
 }
 
+// rather than using the std::minstd_rand for perf-ciritcal stuff, we re-implement the
+// same algortihm:
+// - Using a floating-point type
+// - Without modulo
+// - As a plain inline function operating on simple types.
+// All of this to help the compiler with auto-vectorization.
+typedef double rand_type;
+static inline rand_type minstd_step(rand_type x) {
+  // return (a * x + c) mod m
+  rand_type acc = 0;
+  rand_type m = 2147483647UL;
+  // unrolled shift-add multiplication with modulo after every step, for constant 48271UL.
+  // Maybe we could use a loop and rely on it being unrolled instead...
+  acc += x;
+  if (acc >= m)
+    acc -= m;
+  x = x * 2;
+  if (x >= m)
+    x -= m;
+  x = x * 2;
+  if (x >= m)
+    x -= m;
+  acc += x;
+  if (acc >= m)
+    acc -= m;
+  x = x * 2;
+  if (x >= m)
+    x -= m;
+  acc += x;
+  if (acc >= m)
+    acc -= m;
+  x = x * 2;
+  if (x >= m)
+    x -= m;
+  acc += x;
+  if (acc >= m)
+    acc -= m;
+  x = x * 2;
+  if (x >= m)
+    x -= m;
+  acc += x;
+  if (acc >= m)
+    acc -= m;
+  x = x * 2;
+  if (x >= m)
+    x -= m;
+  x = x * 2;
+  if (x >= m)
+    x -= m;
+  x = x * 2;
+  if (x >= m)
+    x -= m;
+  acc += x;
+  if (acc >= m)
+    acc -= m;
+  x = x * 2;
+  if (x >= m)
+    x -= m;
+  x = x * 2;
+  if (x >= m)
+    x -= m;
+  x = x * 2;
+  if (x >= m)
+    x -= m;
+  x = x * 2;
+  if (x >= m)
+    x -= m;
+  acc += x;
+  if (acc >= m)
+    acc -= m;
+  x = x * 2;
+  if (x >= m)
+    x -= m;
+  acc += x;
+  if (acc >= m)
+    acc -= m;
+  x = x * 2;
+  if (x >= m)
+    x -= m;
+  acc += x;
+  if (acc >= m)
+    acc -= m;
+  x = x * 2;
+  if (x >= m)
+    x -= m;
+  acc += x;
+  if (acc >= m)
+    acc -= m;
+  return acc;
+}
+
+// The vectorized main loop is in this function, to
+// - be able to use __restrict__ pointers.
+// - make the compiler debug output mor readable.
+// - Help the compiler being less confused.
+typedef double accu_type;
+static void doAccumulate(rand_type* __restrict__ localrands,
+                         accu_type val,
+                         accu_type* __restrict__ outbuf,
+                         int ndimensions) {
+  for (int currentdim = 0; currentdim < ndimensions; currentdim++) {
+    rand_type x = minstd_step(localrands[currentdim]);
+    rand_type r = x;
+    localrands[currentdim] = x;
+    accu_type weight = 0.f;
+    // the random number has range 1..0x7FFFFFFF, these values are 1/6 and 2/6 of that.
+    if (r < 715827882)
+      weight = 1.f;  // 1/6'th each, remaining 2/3's stay 0.
+    if (r < 357913941)
+      weight = -1.f;
+    outbuf[currentdim] += weight * val;
+  }
+}
+
 void RandomProjector::performProjection(DQMStore::IGetter& igetter,
                                         DQMStore::IBooker& ibooker,
                                         MonitorElementData::Scope scope) {
@@ -77,7 +189,10 @@ void RandomProjector::performProjection(DQMStore::IGetter& igetter,
 
   auto mes = igetter.getAllContents(meprefix);
 
-  std::vector<double> outbuf(ndimensions, 0);
+  std::vector<accu_type> outbuf_vec(ndimensions, 0);
+  accu_type* outbuf = &outbuf_vec[0];
+  // we will need this later, and should not re-allocate the buffer.
+  auto localrands_vec = std::vector<rand_type>(ndimensions);
 
   int totbins = 0;
 
@@ -103,58 +218,43 @@ void RandomProjector::performProjection(DQMStore::IGetter& igetter,
     }
     // now rand() should be a nice random but reproducible sequence based on the ME name.
 
-    //auto weightdist = std::uniform_real_distribution<double>(-1, 1);
-    auto weightdist = std::normal_distribution<double>(0, 1);
-    auto& weights = this->weights[name];
-    if (weights.size() == 0) {
-      if (kind == MonitorElement::Kind::TH1F) {
-        auto nbins = me->getNbinsX();
-        for (int i = 1; i <= nbins; i++) {
-          for (int currentdim = 0; currentdim < ndimensions; currentdim++) {
-            double weight = weightdist(rand);
-            weights.push_back(weight);
-          }
-        }
-      }
-      if (kind == MonitorElement::Kind::TH2F) {
-        auto nbinsx = me->getNbinsX();
-        auto nbinsy = me->getNbinsY();
-        for (int y = 1; y <= nbinsy; y++) {
-          for (int x = 1; x <= nbinsx; x++) {
-            for (int currentdim = 0; currentdim < ndimensions; currentdim++) {
-              double weight = weightdist(rand);
-              weights.push_back(weight);
-            }
-          }
-        }
-      }
+    // then, we branch off one random generator per dimension to remove a
+    // loop-carried dependency below. That should be enough to get the auto-vectorizer
+    // involved.
+    // We define out own minstd variant with 64 bit state for vectorization (padding!)
+    auto localrands = &localrands_vec[0];
+    for (int currentdim = 0; currentdim < ndimensions; currentdim++) {
+      // offset seed a bit, else they all produce the same (offset) sequence
+      localrands[currentdim] = rand() + 42;
     }
 
-    int ctr = 0;
+    //auto weightdist = std::uniform_real_distribution<double>(-1, 1);
+    //auto weightdist = std::normal_distribution<double>(0, 1);
+    // Using Achlioptas' discrete trick: use sqrt(3) * {+1, 0, -1} as weights.
+    // We can drop the sqrt(3) since we don't care about the linear scaling.
+    // That leaves the distribution between the three, which should be 1/6, 4/6, 1/6.
+    // Sixth's are hard as a power of two, but we can approximate using a lot of bits.
+
+    float* buf = nullptr;
+    int nbins = 0;
     if (kind == MonitorElement::Kind::TH1F) {
-      auto nbins = me->getNbinsX();
-      for (int i = 1; i <= nbins; i++) {
-        double val = me->getBinContent(i);
-        for (int currentdim = 0; currentdim < ndimensions; currentdim++) {
-          double weight = weights[ctr++];
-          outbuf[currentdim] += weight * val;
-        }
-        totbins++;
-      }
+      // unsafe access, but what we do should be sort-of safe.
+      buf = me->getTH1F()->GetArray();
+      // this includes overflows etc.
+      nbins = me->getTH1F()->GetSize();
     }
     if (kind == MonitorElement::Kind::TH2F) {
-      auto nbinsx = me->getNbinsX();
-      auto nbinsy = me->getNbinsY();
-      for (int y = 1; y <= nbinsy; y++) {
-        for (int x = 1; x <= nbinsx; x++) {
-          double val = me->getBinContent(x, y);
-          for (int currentdim = 0; currentdim < ndimensions; currentdim++) {
-            double weight = weights[ctr++];
-            outbuf[currentdim] += weight * val;
-          }
-          totbins++;
-        }
+      buf = me->getTH2F()->GetArray();
+      nbins = me->getTH2F()->GetSize();
+    }
+    if (buf != nullptr) {
+      for (int i = 0; i < nbins; i++) {
+        float val = buf[i];
+        if (isnan(val) || isinf(val))
+          val = 0;
+        doAccumulate(localrands, val, outbuf, ndimensions);
       }
+      totbins += nbins;
     }
   }
 
